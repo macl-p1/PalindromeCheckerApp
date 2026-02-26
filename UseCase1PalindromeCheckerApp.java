d826bc9b9feae1a68aae4a4824a5e24ac5d3bdf6
macl-p1 <gk5390@srmist.edu.in>
2026-02-26 15:37:41 +0530

    Base Application Setup

--- UseCase1PalindromeCheckerApp.java
+++
@@ -1,7 +0,0 @@
-class UseCase1PalindromeCheckerApp{
-    public static void main(String[] args){
-        System.out.println("Welcome to the Palindrome Checker App");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully");
-    }
-}
--- UseCase7PalindromeCheckerApp.java
+++
@@ -1,6 +0,0 @@
-import java.util.*;
-public class UseCase7PalindromeCheckerApp {
-    static void main() {
-
-    }
-}
