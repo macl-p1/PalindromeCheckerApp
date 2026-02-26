fe3b63641694d2e28e5d91b4431bbfc29df17112
macl-p1 <gk5390@srmist.edu.in>
2026-02-26 16:18:44 +0530

    UC3 : Checking with same String

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,13 +1,13 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        String str = "ABA";
-        char[] s = str.toCharArray();
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String x = s;
         boolean not = false;
-        for (int i = 0 ; i < str.length() / 2 ; i++){
-            if (s[i] != s[str.length() - i - 1]){
-                not = true;
-                break;
-            }
+        for (int i = x.length() - 1 ; i >= 0 ; i--){
+            if (s.charAt(i) != x.charAt(x.length() - i - 1)) not = true;
         }
 
         if (!not) System.out.println("It is a Palindrome");
